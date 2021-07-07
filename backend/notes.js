598c2b9c0b9f04fcbe1c00ca9ec30ee2c307c119
Rappson <axel.grolier@gmail.com>
2021-07-07 14:53:46 +0200

    mise en note les informations

---
+++ backend/notes.js
@@ -0,0 +1,18 @@
+const url = "localhost:4200/api";
+
+
+// modèle de données pour une sauce
+const schema = {
+    id = "identifiant du produit",
+    userId = "identifiant de l'utilisateur",
+    name = "nom de la sauce",
+    manuf = "fabricant de la sauce",
+    description = "description de la sauce",
+    mainPepper = "ingrédient principal",
+    image = "url de l'image",
+    heat = "note de la sauce",
+    likes = "nombre de like",
+    dislikes = "nombre de dislikes",
+    userLiked = "tableau d'identifiants qui à liké",
+    userDisliked ="/// qui a disliké"
+}
