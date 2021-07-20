a65c9097ebb13497b214149c446ad821fe81b47f
Rappson <axel.grolier@gmail.com>
2021-07-20 13:11:09 +0200

    route get

--- backend/app.js
+++ backend/app.js
@@ -33,26 +33,10 @@ app.use((req, res, next) => {
     .catch(error => res.status(400).json({error}));
   });
 
-app.use((req, res, next) => {
-    const sauces = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(sauces);
+app.use('api/sauces',(req, res, next) => {
+    Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({error}));
   });
 
 
