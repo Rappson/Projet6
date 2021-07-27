0fddb52b3ce53909152ff4e5425944ed2ef0c26c
Rappson <axel.grolier@gmail.com>
2021-07-27 23:24:19 +0200

    mise en place du model utilisateur

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -5,13 +5,13 @@ const sauceSchema = mongoose.Schema({
     name: { type: String, required: true},
     manuf: { type: String, required: true},
     description: {type: String, required: true},
-    mainPepper: {type: String, required: true},
-    image: { type: String, required: true},
-    heat: {type: Number, required: true},
-    likes: {type: Number, required: true},
-    dislikes: {type: Number, required: true},
-    userLiked: {type: String, required: true},
-    userDisliked: {type: String, required: true}
+    mainPepper: {type: String, required: false},
+    image: { type: String, required: false},
+    heat: {type: Number, required: false},
+    likes: {type: Number, required: false},
+    dislikes: {type: Number, required: false},
+    userLiked: {type: String, required: false},
+    userDisliked: {type: String, required: false}
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
