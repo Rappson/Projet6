c1852469c25442f1adb050bb27e4d8e34244c86f
Rappson <axel.grolier@gmail.com>
2021-08-23 21:40:42 +0200

    correction des likes

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -9,6 +9,7 @@ exports.createSauce = (req, res, next) => {
     const sauce = new Sauce({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        // METTRE LES LIKES
     });
     // je l'enregistre dans la base de donnée
     sauce.save()
@@ -53,6 +54,35 @@ exports.deleteSauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
+function gestionLikes(req, sauce) {
+    /*si l'user aime la sauce:
+    je verifie dans les tableaux qu'il n'est pas présent sinon je le supprime des tableaux
+    je l'ajoute dans le tableau correspondant
+    */
+
+    if (req.body.like === 1) {
+        // position dans le tableau des likes et des dislikes de l'userId
+        let positionOnLike = sauce.usersLiked.indexOf(req.body.userId);
+        let positionOnDisLike = sauce.usersDisliked.indexOf(req.body.userId);
+
+        // je supprime l'userId des tableaux
+        if (positionOnLike != -1) {
+            sauce.usersLiked.splice(positionOnLike, 1);
+            console.log('id supprimé du tableau des likes');
+        } else if (positionOnDisLike != -1) {
+             sauce.usersDisliked.splice(positionOnDisLike, 1);
+            console.log('id supprimé du tableau des dislikes');
+        }
+        sauce.usersLiked.push(req.body.userId)
+
+        sauce.likes = sauce.usersLiked.length
+        sauce.save()
+    }
+
+    // résultat de la requete
+    console.log(sauce);
+}
+
 exports.like = (req, res, next) => {
     // Si j'aime = 1, l'utilisateur aime la sauce. 
     // Si j'aime = 0, l'utilisateur annule ce qu'il aime ou ce qu'il n'aime pas.
@@ -62,41 +92,12 @@ exports.like = (req, res, next) => {
         .then(sauce => {
             // userId et résultat du like
             console.log(req.body);
-            let tabLikes = sauce.usersLiked;
-            let tabDislikes = sauce.usersDisliked;
-           
-            function gestionLikes() {
-                /*si l'user aime la sauce:
-                je verifie dans les tableaux qu'il n'est pas présent sinon je le supprime des tableaux
-                je l'ajoute dans le tableau correspondant
-                */
-                if (req.body.id === 1) {
-                    // position dans le tableau des likes et des dislikes de l'userId
-                    let positionOnLike = tabLikes.indexOf(req.body.userId);
-                    let positionOnDisLike = tabDislikes.indexOf(req.body.userId);
 
-                    // je supprime l'userId des tableaux
-                    if (positionOnLike != -1) {
-                        // tabLikes.splice(0, positionOnLike);
-                        delete tabLikes[ positionOnLike ]
-                    } else if (positionOnDisLike != -1) {
-                        // tabDislikes.splice(0, positionOnDisLike);
-                        delete tabDislikes[ positionOnDisLike ]
-                    }
-                    sauce.usersLiked.push(req.body.userId)
-                }
-                
-                sauce.likes = sauce.usersLiked.length;
-                sauce.dislikes = sauce.usersDisliked.length;
-                // résultat de la requete
-                console.log(sauce);
-                sauce.save();
-            }
-            gestionLikes();
-            console.log(tabLikes);
-
+            gestionLikes(req, sauce);
+            res.status(200).json({ message: 'like posté !' })
         })
-        .then(() => res.status(200).json({ message: "like posté" }))
-        .catch(error => res.status(400).json({ error }));
-
+        .catch(error => {
+            res.status(400).json({ error })
+            console.log(error);
+        });
 };
