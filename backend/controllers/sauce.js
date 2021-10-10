3862b0cde52393cd3febe6edf9cb65e140ae64dd
Rappson <axel.grolier@gmail.com>
2021-10-10 17:55:30 +0200

    rs

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const likes = require('../services/like');
 const User = require('../models/user');
 
-const JoiCreate = require('../services/joi-create');
+const validationJoi = require('../services/joi-create');
 
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
 
-    const createValidate = JoiCreate.validate({
+    const createValidate = validationJoi.validate({
         ...sauceObject,
         likes: 0,
         dislikes: 0,
@@ -40,51 +40,36 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-    const data = req.body;
-    const sauceValidate = JoiCreate.validate({
-        ...data,
-        _id: req.params.id,
-    })
-    const sauceObject =
-    {
-        ...sauceValidate.value,
-    }
-
-
-    Sauce.findOne({ _id: req.params.id })
+    Sauce.findOne({ _id: req.body.id })
         .then(sauce => {
-            console.log(sauce);
-            const authId = req.body.onlineUser;
-            const sauceUserId = sauce.userId;
-            console.log(authId);
-            User.findOne({_id : req.body.onlineUser})
-            .then(user =>{
-                console.log(user);
+            /* SI il y a un changement d'image
+            modifié le body et changer l'image
+            SINON
+            change juste le body 
+            */
+            const modifiedObject = validationJoi.validate({
+                ...req.body,
+                _id: req.body.id
             })
-            if (sauceUserId === authId) {
-                const img = sauce.imageUrl;
-
-                if (req.file && img != `${req.protocol}://${req.get('host')}/images/${req.file.filename}`) {
-                    // si l'image change
-                    const filename = sauce.imageUrl.split('/images/')[ 1 ];
-                    fs.unlink(`images/${filename}`, () => {
-                        Sauce.updateOne({ _id: sauceValidate.value._id },
-                            {
-                                ...sauceObject,
-                                imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-                                _id: sauceValidate.value._id
-                            })
-                            .then(() => res.status(200).json({ message: 'Sauce modifiée !!' }))
-                            .catch(error => res.status(400).json({ error }));;
+            /* 
+            CE QUI ME RESTE A FAIRE :
+            VERIFIER si l'id est correct
+            */
+            if (req.file) {
+                const filename = sauce.imageUrl.split('/images/')[ 1 ];
+                fs.unlink(`images/${filename}`, () => {
+                    sauce.updateOne({ _id: modifiedObject.value._id }, {
+                        ...modifiedObject.value,
+                        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+                        _id: modifiedObject.value._id
                     })
-                } else {
-                    // si l'image ne change pas
-                    Sauce.updateOne({ _id: sauceValidate.value._id }, { ...sauceObject, _id: sauceValidate.value._id })
-                        .then(() => res.status(200).json({ message: 'Sauce modifiée !!' }))
-                        .catch(error => res.status(400).json({ error }));
-                }
-            } else{
-                console.error( `You are not allowed to do this action !!!`);
+                    .then(() => res.status(200).json({message: 'Sauce modifié !'}))
+                    .catch(error => res.status(400).json({error}));
+                })
+            } else {
+                sauce.updateOne({_id: modifiedObject.value._id}, {...modifiedObject.value, _id: modifiedObject.value._id})
+                .then(() => res.status(200).json({message: 'Sauce modifié !'}))
+                .catch(error => res.status(400).json({error}));
             }
         })
 };
